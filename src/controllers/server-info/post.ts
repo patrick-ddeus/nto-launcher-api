b1550402f3e4b88d6c3143ee83ba8c7559dfd27e
Patrick <patrickdev4@gmail.com>
2025-05-09 17:40:46 -0300

    feat: add auto commit by github

--- src/controllers/server-info/post.ts
+++ src/controllers/server-info/post.ts
@@ -1,27 +1,57 @@
 import { Request, Response } from 'express';
-import { writeFile } from 'fs/promises';
-import path from 'path';
-import { webSocket } from '../../app';
+import axios from 'axios';
 
-const serverInfo = process.env.SERVER_INFO_FILE;
+const GITHUB_OWNER = 'patrick-ddeus';
+const GITHUB_REPO = 'nto-launcher-api';
+const FILE_PATH = 'client_info.json';
+const BRANCH = 'main';
 
 export default async (req: Request, res: Response) => {
-  try {
-    const filePath = path.join(process.cwd(), serverInfo);
-    const data = req.body;
+  const githubToken = process.env.GITHUB_TOKEN;
+  const apiUrl = `https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${FILE_PATH}`;
 
+  try {
+    const data = req.body;
     if (!data) {
       res.status(400).json({ error: 'Nenhum dado fornecido' });
-      return;
+      return
     }
 
     data.last_update = new Date();
 
-    await writeFile(filePath, JSON.stringify(data, null, 2), 'utf-8');
-    webSocket.notifyClient(1, 'true');
-    res.status(200).json({ message: 'Dados salvos com sucesso' });
+    const { data: fileData } = await axios.get(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${githubToken}`,
+        Accept: 'application/vnd.github+json',
+      },
+      params: { ref: BRANCH },
+    });
+
+    const updatedContent = Buffer.from(JSON.stringify(data, null, 2)).toString(
+      'base64'
+    );
+
+    await axios.put(
+      apiUrl,
+      {
+        message: 'update client_info.json via API',
+        content: updatedContent,
+        sha: fileData.sha,
+        branch: BRANCH,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${githubToken}`,
+          Accept: 'application/vnd.github+json',
+        },
+      }
+    );
+
+    res.status(200).json({ message: 'Arquivo atualizado com sucesso' });
+    return;
   } catch (error) {
-    console.error('Erro ao salvar os dados:', error);
+    console.error('Erro ao salvar os dados:', error?.response?.data || error);
     res.status(500).json({ error: 'Erro ao salvar os dados' });
+    return;
   }
 };
